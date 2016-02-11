c3b037f899d1f4b0f533d28cbaf16094520d5876
amq <amq@users.noreply.github.com>
2016-02-12 00:30:26 +0100

    Fix codecov

---
+++ lib.h
@@ -0,0 +1,7 @@
+#ifndef MYFIND_LIB_H
+
+int mul(int a, int b);
+
+#define MYFIND_LIB_H
+
+#endif //MYFIND_LIB_H
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "lib.h"
 
 int main(void) {
   printf("Hello\n");
-  printf("Hello\n");
+  printf("%d\n", mul(2, 3));
 }
