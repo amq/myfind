748e20b4afdaa83dca66a1aff1b31ffbcd12507e
amq <amq@users.noreply.github.com>
2016-02-27 00:01:02 +0100

    Accept both files and directories as an input, properly handle opendir errors

--- main.c
+++ main.c
@@ -7,15 +7,29 @@
 
 int do_dir(const char *location, char *params[]);
 int do_file(const char *location, char *params[]);
-int print_usage();
+void print_usage();
 
 int main(int argc, char *argv[]) {
+  struct stat attr;
+
+  /* minimum of 3 input parameters are required */
+  /* myfind <file or directory> [ <aktion> ] */
   if (argc < 3) {
     print_usage();
     return EXIT_FAILURE;
   }
 
-  do_dir(argv[1], argv);
+  /* both files and directories are accepted as an input */
+  if (lstat(argv[1], &attr) == 0) {
+    if (S_ISDIR(attr.st_mode)) {
+      do_dir(argv[1], argv);
+    } else if (S_ISREG(attr.st_mode)) {
+      do_file(argv[1], argv);
+    }
+  } else {
+    printf("%s: lstat(%s): %s\n", argv[0], argv[1], strerror(errno));
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -25,17 +39,17 @@ int do_dir(const char *location, char *params[]) {
   struct dirent *entry;
   struct stat attr;
 
-  /* open the directory */
   dir = opendir(location);
 
   if (!dir) {
-    printf("opendir(%s): %s\n", location, strerror(errno));
-    /* continue even if some entries fail */
+    printf("%s: opendir(%s): %s\n", params[0], location, strerror(errno));
+    return EXIT_FAILURE;
   }
 
   while ((entry = readdir(dir))) {
+
     /* skip '.' and '..' */
-    if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name)) {
+    if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0) {
       continue;
     }
 
@@ -43,7 +57,7 @@ int do_dir(const char *location, char *params[]) {
     char *path = malloc(strlen(location) + strlen(entry->d_name) + 2);
 
     if (!path) {
-      printf("malloc(): %s\n", strerror(errno));
+      printf("%s: malloc(): %s\n", params[0], strerror(errno));
       return EXIT_FAILURE;
     }
 
@@ -54,43 +68,47 @@ int do_dir(const char *location, char *params[]) {
     do_file(path, params);
 
     /* if the entry is a directory, call the function recursively */
-    if (!lstat(path, &attr)) {
+    if (lstat(path, &attr) == 0) {
       if (S_ISDIR(attr.st_mode)) {
         do_dir(path, params);
       }
     } else {
-      printf("lstat(%s): %s\n", location, strerror(errno));
+      printf("%s: lstat(%s): %s\n", params[0], location, strerror(errno));
     }
 
     free(path);
   }
 
-  if (closedir(dir)) {
-    printf("closedir(%s): %s\n", location, strerror(errno));
+  if (closedir(dir) != 0) {
+    printf("%s: closedir(%s): %s\n", params[0], location, strerror(errno));
   }
 
   return EXIT_SUCCESS;
 }
 
 int do_file(const char *location, char *params[]) {
+
   for (int i = 0; params[i]; i++) {
-    if (!strcmp(params[i], "-print")) {
+
+    if (strcmp(params[i], "-print") == 0) {
       printf("%s\n", location);
+    }
+    if (strcmp(params[i], "-ls") == 0) {
+      /* TODO */
     }
   }
 
   return EXIT_SUCCESS;
 }
 
-int print_usage() {
-  printf("myfind <file or directory> [ <aktion> ]\n"
-                 "-user <name>|<uid>\n"
-                 "-name <pattern>\n"
-                 "-type [bcdpfls]\n"
-                 "-print\n"
-                 "-ls\n"
-                 "-nouser\n"
-                 "-path\n");
+void print_usage() {
 
-  return EXIT_FAILURE;
+  printf("myfind <file or directory> [ <aktion> ]\n"
+         "-user <name>|<uid>\n"
+         "-name <pattern>\n"
+         "-type [bcdpfls]\n"
+         "-print\n"
+         "-ls\n"
+         "-nouser\n"
+         "-path\n");
 }
